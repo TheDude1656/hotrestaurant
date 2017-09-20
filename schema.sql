4b48454fdfb291b3bf91c568e1b91d56f90475f9
Kyle Atchison <katchison@gmail.com>
2017-09-19 20:12:37 -0500

    changes to sql table

--- schema.sql
+++ schema.sql
@@ -4,10 +4,11 @@ CREATE database reservationsDB;
 USE reservationsDB;
 
 CREATE TABLE customer (
-    position INT NOT NULL,
+    position INT AUTO_INCREMENT NOT NULL,
     customerName VARCHAR(100) NOT NULL,
     customerEmail VARCHAR(100) NOT NULL,
     customerID VARCHAR(100) NOT NULL,
     phoneNumber INT NOT NULL,
+    waitlist BOOLEAN DEFAULT 1,
     PRIMARY KEY (position)
 );
